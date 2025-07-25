22b6aa8539d426b4da3819fdf7b85ef229cb3d16
ayaangoel7 <119551557+ayaangoel7@users.noreply.github.com>
2025-07-25 11:26:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'
+app.secret_key = os.environ.get('SECRET_KEY', 'fallbacksecret123')
 DATABASE = 'bank.db'
 
 
